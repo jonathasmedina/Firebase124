b17624b8830aaa6603107ab55359a6cc416ce19f
jonathasmedina <emaildojonathas@gmail.com>
2019-05-31 08:49:14 -0400

    -

--- app/src/main/java/com/example/firebase124/MainActivity.java
+++ app/src/main/java/com/example/firebase124/MainActivity.java
@@ -150,6 +150,7 @@ public class MainActivity extends AppCompatActivity {
     private void inicializarFirebase() {
         FirebaseApp.initializeApp(MainActivity.this);
         firebaseDatabase = FirebaseDatabase.getInstance();
+        firebaseDatabase.setPersistenceEnabled(true);
         databaseReference = firebaseDatabase.getReference();
 
     }
